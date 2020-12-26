ed6c8b0acd12b8682d4cd7991fe9cb0f9c31c4ab
Esra Guc <esra.guc@ed.ac.uk>
2020-12-26 23:06:16 +0000

    update Plot1

--- Plot1.R
+++ Plot1.R
@@ -1,4 +1,7 @@
+#Read RDS data
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
+#Total Emission per year
 Emissions<- tapply (NEI$Emissions, NEI$year, sum)
+# Plot the graph
 barplot(Emissions, xlab="Year", ylab="Total Emission(ton)", main="Total Emission")
